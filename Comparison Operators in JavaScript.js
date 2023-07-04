252680262ea3bbf1eb6b357d7edf44d3cc7b29d6
MANIKANDAN.C <112545424+manikandanmech@users.noreply.github.com>
2023-07-04 22:31:52 +0530

    Add files via upload

---
+++ Comparison Operators in JavaScript.js
@@ -0,0 +1,23 @@
+/*
+Comparison Operators in JavaScript:
+
+Equality (==): This operator compares two values to see if they are equal.
+
+Inequality (!=): This operator compares two values to see if they are not equal.
+
+=== operator, on the other hand, is a strict equality operator and does not perform type coercion. It will only return true if the values being compared have the same type and value.
+
+	Operator	Usage
+	==	qual to
+	===	equal value and equal type
+    !=	not equal
+	!==	not equal value or not equal type */
+
+//Comparison Operators
+ 
+let a=10;
+let b="25";
+console.log(a==b);
+console.log(a===b);
+console.log(a!=b);
+console.log(a!==b);
