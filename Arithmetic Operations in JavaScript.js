4edc2c5dcd7ebb57a2ea18f462a0882c3dc6a0f0
MANIKANDAN.C <112545424+manikandanmech@users.noreply.github.com>
2023-07-04 21:39:48 +0530

    Add files via upload

---
+++ Arithmetic Operations in JavaScript.js
@@ -0,0 +1,25 @@
+/*Performing Arithmetic Operations in JavaScript:
+    In JavaScript, arithmetic operations are used to perform mathematical calculations and manipulate numerical values. The basic arithmetic operations include
+    Operator	Usage
+	+	Addition
+	-	Subtraction
+	*	Multiplication
+	**	Exponentiation (2016)
+	/	Division
+	%	Modulus (Remainder)
+	++	Increment
+	--	Decrement */
+
+  //Arithmetic Operation
+let a=100;
+let b=20;
+let c;
+c=a+b;
+c=a-b;
+c=a*b;
+c=a/b;
+c=a%b;
+c=2**3;//2016
+c=++a;
+c=--b;
+console.log(c);
