d17f74a8c6b62b8a8d86d51923a4838cba2f1acf
MANIKANDAN.C <112545424+manikandanmech@users.noreply.github.com>
2023-07-04 17:00:40 +0530

    Add files via upload

---
+++ Introduction.js.js
@@ -0,0 +1,33 @@
+/*  JavaScript is a dynamic programming language  >>>>
+      that's used for web development,It is used both on the client-side and server-side that allows you to make web pages interactive.
+ Where HTML and CSS are languages that give structure and style to web pages, JavaScript gives web pages interactive elements that engage a user.
+ It is an interpreted programming language with object-oriented capabilities 
+  
+ All popular web browsers support JavaScript as they provide built-in execution environments.
+
+ JavaScript is an object-oriented programming language that uses prototypes rather than using classes for inheritance.
+
+ It is a light-weighted and interpreted language.
+ 
+ */
+
+
+ /* In this simple program that prints "Hey Mani " on the screen. Since it's a very simple program, 
+ this program is often used to introduce a new programming language to beginners.
+In JavaScript, you can use the alert() function to display an alert box with a message to the user. The basic syntax for the alert() function is as follows: */
+ 
+/*
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Tutor Joes</title>
+</head>
+<body>
+	<script>
+		alert("Tutor Joes");
+	</script>
+</body>
+</html> */
