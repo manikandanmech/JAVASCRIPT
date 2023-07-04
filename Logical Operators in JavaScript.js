3860a45bf7bf5342485edcec3f75c99a5569f2f6
MANIKANDAN.C <112545424+manikandanmech@users.noreply.github.com>
2023-07-04 22:41:31 +0530

    Add files via upload

---
+++ Logical Operators in JavaScript.js
@@ -0,0 +1,19 @@
+//Logical Operators in JavaScript 
+
+/*
+&&	logical and
+||	logical or
+!	  logical not
+*/
+
+//35-100
+let runs=45;
+console.log(runs>=35 && runs<=100);
+ 
+let a=5;
+//2,5
+console.log(a==2 || a==5);
+ 
+a=false;
+console.log(!a);
+ 
