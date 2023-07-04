4a1b858f5b33f94d6335ba7c6a3863aec650ad3e
MANIKANDAN.C <112545424+manikandanmech@users.noreply.github.com>
2023-07-04 22:36:46 +0530

    Add files via upload

---
+++ Relational Operators in JavaScript.js
@@ -0,0 +1,16 @@
+//Relational Operators in JavaScript
+ 
+/*
+>	greater than
+<	less than
+>=	greater than or equal to
+<=	less than or equal to
+*/
+ 
+let a=10;
+let b=20;
+
+console.log("Greater : ",a>b);
+console.log("Lesser  : ",a<b);
+console.log("Greater Than Equal  : ",a>=b);
+console.log("Greater Than Equal  : ",a<=b);
