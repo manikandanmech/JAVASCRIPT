1d05c08140b3cf015416e318880191d35fb2f73c
MANIKANDAN.C <112545424+manikandanmech@users.noreply.github.com>
2023-07-27 22:04:57 +0530

    Add files via upload

---
+++ While Loop in JavaScript.js
@@ -0,0 +1,15 @@
+/*
+                                   While Loop in JavaScript
+A while loop is a control flow statement that allows you to repeatedly execute a block of code as long as a specified condition evaluates to true. It is one of the fundamental loop constructs in JavaScript and is used when the number of iterations is not known beforehand.
+
+Syntax:
+    while (condition) {
+        // Code to be executed as long as the condition is true
+    }
+*/
+
+let i = 1;
+while (i < 20) {
+console.log(i);
+i++;
+}
